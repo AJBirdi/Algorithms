29fdfce18b242aa32c6dd0f5cabc3d6b3caa5c53
AJ Birdi <angadjot@aim.com>
2017-02-17 13:15:18 -0500

    Restructured code and fixed indexing issues

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,15 +1,12 @@
-//Built in accordance to the specification at http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
+//Built in accordance to the specification at 
+//http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
 
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.lang.IllegalArgumentException;
 
 public class Percolation {	
-	//Converts row and column to a number that can access a single dimensional array
-	private int location; 
 	private int gridSize;
+	private int openSiteCount;
 	
 	private WeightedQuickUnionUF WQU;
 	
@@ -22,11 +19,11 @@ public class Percolation {
 		else{
 			gridSize = n;
 			//Creates grid with 2 extra nodes that will function as virtual top and bottoms 
-			//	that are connected to all top and all bottom nodes
+			// that are connected to all top and all bottom nodes
 			grid = new boolean[(n * n) + 2];
 			WQU = new WeightedQuickUnionUF((gridSize * gridSize) + 2);
 			
-			//Makes extra nodes connect to top and bottom rows
+			//Makes virtual nodes connect to top and bottom rows
 			for(int x = 0; x < gridSize; x++){
 				WQU.union(gridSize * gridSize, x);
 				WQU.union(((gridSize * gridSize) + 1), ((gridSize * (gridSize -1)) + x));
@@ -39,40 +36,52 @@ public class Percolation {
 		if((row == 0 || column == 0) || (row > gridSize || column > gridSize)){
 			throw new IndexOutOfBoundsException();
 		}
-		
+				
 		if(!isOpen(row, column)){
-			location = ((row - 1) * gridSize) + (column - 1);
-			StdOut.println("This site was opened [" + row + ", " + column + "].");
-			grid[location] = true;
-			
-			//Checks adjacent sites to see if they're open and connect to them if they are
-			if((row - 1) >= 1 && isOpen(row - 1, column) && (location - gridSize) >= 0){
-				WQU.union(location, location - gridSize);
+			//Creates number that can be used to access a single dimensional array that represents
+			// two dimensional data
+			int gridLocation = ((row - 1) * gridSize) + (column - 1);
+			grid[gridLocation] = true;
+			openSiteCount++;
+						
+			//Checks if top element is open and connects if it is
+			if((row - 1) >= 0 && (gridLocation - gridSize) >= 0){
+				if((row - 1) > 0 && isOpen(row - 1, column)){
+					WQU.union(gridLocation, (gridLocation - gridSize));
+				}
 			}
-			else if((row + 1 <= gridSize) && isOpen(row + 1, column) && (location + gridSize <= 24)){
-				WQU.union(location, location + gridSize);
+			//Checks if bottom element is open and connects if it is
+			if(row + 1 <= gridSize && gridLocation + gridSize <= gridSize * gridSize){
+				if(row + 1 <= gridSize && isOpen(row + 1, column)){
+					WQU.union(gridLocation, gridLocation + gridSize);
+				}
 			}
-			else if((column + 1 <= gridSize) && isOpen(row, column + 1) && (location + 1 <= 24)){
-				WQU.union(location, location + 1);
+			//Checks if right element is open and connects if it is
+			if((column + 1 <= gridSize) && (gridLocation + 1 <= (gridSize * gridSize - 1))){
+				if(column + 1 <= gridSize && isOpen(row, column + 1)){
+					WQU.union(gridLocation, gridLocation + 1);
+				}
 			}
-			else if((column - 1 >= 1) && isOpen(row, column - 1) && (location - 1) >= 0){
-				WQU.union(location, location - 1);
+			//Checks if left element is open and connects if it is
+			if((column - 1 >= 0) && (gridLocation - 1) >= 0){
+				if(column - 1 > 0 && isOpen(row, column - 1)){
+					WQU.union(gridLocation, gridLocation - 1);
+				}
 			}
 		}
 		else{
 			StdOut.println("Cannot open site.");
 		}
-		
 	}
 	
 	//Checks to see if a site is open
 	public boolean isOpen(int row, int column) {
-		if((row == 0 || column == 0) || (row > gridSize || column > gridSize)){
+		if((row <= 0 || column <= 0) || (row > gridSize || column > gridSize)){
 			throw new IndexOutOfBoundsException();
 		}
 		
-		location = ((row - 1) * gridSize) + (column - 1);
-		if(grid[location] == false){
+		int gridLocation = ((row - 1) * gridSize) + (column - 1);
+		if(grid[gridLocation] == false){
 			return false;
 		}
 		else{
@@ -82,12 +91,12 @@ public class Percolation {
 	
 	//Checks to see if a site is closed
 	public boolean isFull(int row, int column) {
-		if((row == 0 || column == 0) || (row > gridSize || column > gridSize)){
+		if((row <= 0 || column <= 0) || (row > gridSize || column > gridSize)){
 			throw new IndexOutOfBoundsException();
 		}
 		
-		location = ((row - 1) * gridSize) + (column - 1);
-		if(grid[location] == false){
+		int gridLocation = ((row - 1) * gridSize) + (column - 1);
+		if(grid[gridLocation] == false){
 			return true;
 		}
 		else{
@@ -97,19 +106,11 @@ public class Percolation {
 	
 	//Returns number of open sites
 	public int numberOfOpenSites() {
-		int count = 0;
-	
-		for(int x = 0; x < (gridSize * gridSize); x++){
-			if(grid[x] == true){
-				count++;
-			}
-		}
-		return count;
+		return openSiteCount;
 	}
 	
 	//Checks if there is a path from the any of the top nodes to any of the bottom nodes
 	public boolean percolates() {
-		//Check if the virtual sites are connected to each other
 		return WQU.connected((gridSize * gridSize), ((gridSize * gridSize) + 1));
 	}	
 }
