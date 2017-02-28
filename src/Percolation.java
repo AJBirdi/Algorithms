d81d8d4a4e51eeaaa2aad91a6b3151d4383cf197
AJ Birdi <angadjot@aim.com>
2017-02-28 15:21:00 -0500

    PercolationStats constructor now makes objects instead of main, updated open method and isFull

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,114 +1,110 @@
 // Built in accordance to the specification at 
 // http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
 
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {	
-	private int gridSize;
-	private int openSiteCount;
-	
-	private WeightedQuickUnionUF weightedQuickUnion;
-	
-	private boolean[] grid;
-	
-	public Percolation(int n) {
-		if (n <= 0){
-			throw new IllegalArgumentException();
-		}
-		else {
-			gridSize = n;
-			// Creates grid with 2 extra nodes that will function as virtual top and bottoms 
-			//  that are connected to all top and all bottom nodes
-			grid = new boolean[(n * n) + 2];
-			weightedQuickUnion = new WeightedQuickUnionUF((gridSize * gridSize) + 2);
-			
-			//Makes virtual nodes connect to top and bottom rows
-			for (int x = 0; x < gridSize; x++){
-				weightedQuickUnion.union(gridSize * gridSize, x);
-				weightedQuickUnion.union(((gridSize * gridSize) + 1), ((gridSize * (gridSize -1)) + x));
-			}
-		}
-	}
-	
-	// Open a site then connect it to adjacent sites that are also open
-	public void open(int row, int column) {
-		if ((row == 0 || column == 0) || (row > gridSize || column > gridSize)){
-			throw new IndexOutOfBoundsException();
-		}
-				
-		if (!isOpen(row, column)) {
-			// Creates number that can be used to access a single dimensional array that represents
-			//  two dimensional data
-			int gridLocation = ((row - 1) * gridSize) + (column - 1);
-			grid[gridLocation] = true;
-			openSiteCount++;
-						
-			// Checks if top element is open and connects if it is
-			if ((row - 1) >= 0 && (gridLocation - gridSize) >= 0) {
-				if ((row - 1) > 0 && isOpen(row - 1, column)) {
-					weightedQuickUnion.union(gridLocation, (gridLocation - gridSize));
-				}
-			}
-			// Checks if bottom element is open and connects if it is
-			if (row + 1 <= gridSize && gridLocation + gridSize <= gridSize * gridSize) {
-				if (row + 1 <= gridSize && isOpen(row + 1, column)) {
-					weightedQuickUnion.union(gridLocation, gridLocation + gridSize);
-				}
-			}
-			// Checks if right element is open and connects if it is
-			if ((column + 1 <= gridSize) && (gridLocation + 1 <= (gridSize * gridSize - 1))) {
-				if (column + 1 <= gridSize && isOpen(row, column + 1)) {
-					weightedQuickUnion.union(gridLocation, gridLocation + 1);
-				}
-			}
-			// Checks if left element is open and connects if it is
-			if ((column - 1 >= 0) && (gridLocation - 1) >= 0) {
-				if (column - 1 > 0 && isOpen(row, column - 1)) {
-					weightedQuickUnion.union(gridLocation, gridLocation - 1);
-				}
-			}
-		}
-		else {
-			StdOut.println("Cannot open site.");
-		}
-	}
-	
-	// Checks to see if a site is open
-	public boolean isOpen(int row, int column) {
-		if ((row <= 0 || column <= 0) || (row > gridSize || column > gridSize)){
-			throw new IndexOutOfBoundsException();
-		}
-		
-		int gridLocation = ((row - 1) * gridSize) + (column - 1);
-		if (grid[gridLocation] == false){
-			return false;
-		}
-		else {
-			return true;
-		}		
-	}
-	
-	// Checks to see if a site is connected to the top virtual node
-	public boolean isFull(int row, int column) {
-		if ((row <= 0 || column <= 0) || (row > gridSize || column > gridSize)){
-			throw new IndexOutOfBoundsException();
-		}
-		
-		int gridLocation = ((row - 1) * gridSize) + (column - 1);
-		return weightedQuickUnion.connected(gridLocation, gridSize * gridSize);
-	}
-	
-	// Returns number of open sites
-	public int numberOfOpenSites() {
-		return openSiteCount;
-	}
-	
-	// Checks if there is a path from the any of the top nodes to any of the bottom nodes
-	public boolean percolates() {
-		if (gridSize == 1){
-			return false;
-		}
-		return weightedQuickUnion.connected((gridSize * gridSize), ((gridSize * gridSize) + 1));
-	}	
+    private int gridSize;
+    private int openSiteCount;
+
+    private WeightedQuickUnionUF weightedQuickUnion;
+
+    private boolean[] grid;
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            gridSize = n;
+            // Creates grid with 2 extra nodes that will function as virtual top and bottoms
+            //  that are connected to all top and all bottom nodes
+            grid = new boolean[(n * n) + 2];
+            weightedQuickUnion = new WeightedQuickUnionUF((gridSize * gridSize) + 2);
+
+            // Makes virtual nodes connect to top and bottom rows
+            for (int x = 0; x < gridSize; x++) {
+                weightedQuickUnion.union(gridSize * gridSize, x);
+                weightedQuickUnion.union(((gridSize * gridSize) + 1), ((gridSize * (gridSize -1)) + x));
+            }
+        }
+    }
+
+    // Open a site then connect it to adjacent sites that are also open
+    public void open(int row, int column) {
+        if ((row == 0 || column == 0) || (row > gridSize || column > gridSize)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (!isOpen(row, column)) {
+            // Creates number that can be used to access a single dimensional array that represents
+            //  two dimensional data
+            int gridLocation = ((row - 1) * gridSize) + (column - 1);
+            grid[gridLocation] = true;
+            openSiteCount++;
+
+            // Checks if top element is open and connects if it is
+            if ((row - 1) >= 0 && (gridLocation - gridSize) >= 0) {
+                if ((row - 1) > 0 && isOpen(row - 1, column)) {
+                    weightedQuickUnion.union(gridLocation, (gridLocation - gridSize));
+                }
+            }
+            // Checks if bottom element is open and connects if it is
+            if (row + 1 <= gridSize && gridLocation + gridSize <= gridSize * gridSize) {
+                if (row + 1 <= gridSize && isOpen(row + 1, column)) {
+                    weightedQuickUnion.union(gridLocation, gridLocation + gridSize);
+                }
+            }
+            // Checks if right element is open and connects if it is
+            if ((column + 1 <= gridSize) && (gridLocation + 1 <= (gridSize * gridSize - 1))) {
+                if (column + 1 <= gridSize && isOpen(row, column + 1)) {
+                    weightedQuickUnion.union(gridLocation, gridLocation + 1);
+                }
+            }
+            // Checks if left element is open and connects if it is
+            if ((column - 1 >= 0) && (gridLocation - 1) >= 0) {
+                if (column - 1 > 0 && isOpen(row, column - 1)) {
+                    weightedQuickUnion.union(gridLocation, gridLocation - 1);
+                }
+            }
+        }
+    }
+
+    // Checks to see if a site is open
+    public boolean isOpen(int row, int column) {
+        if ((row <= 0 || column <= 0) || (row > gridSize || column > gridSize)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int gridLocation = ((row - 1) * gridSize) + (column - 1);
+        return grid[gridLocation];
+    }
+
+    // Checks to see if a site is connected to the top virtual node
+    public boolean isFull(int row, int column) {
+        if ((row <= 0 || column <= 0) || (row > gridSize || column > gridSize)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int gridLocation = ((row - 1) * gridSize) + (column - 1);
+        if (row > 1) {
+            return weightedQuickUnion.connected(gridLocation, gridSize * gridSize);
+        }
+        else {
+               return isOpen(row, column);
+        }
+    }
+
+    // Returns number of open sites
+    public int numberOfOpenSites() {
+        return openSiteCount;
+    }
+
+    // Checks if there is a path from the any of the top nodes to any of the bottom nodes
+    public boolean percolates() {
+        if (gridSize == 1) {
+            return false;
+        }
+        return weightedQuickUnion.connected((gridSize * gridSize), ((gridSize * gridSize) + 1));
+    }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,73 +1,79 @@
-//Built in accordance to the specification at 
-//http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
+// Built in accordance to the specification at
+// http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
 
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import java.lang.Math;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PercolationStats {
-	
-	private int gridSize;
-	private int trialCount;
-	private double[] percThreshold;
-	
-	public PercolationStats(int n, int trials){
-		if(n <= 0 || trials <= 0){
-			throw new IllegalArgumentException();
-		}
-		else{
-			gridSize = n;
-			trialCount = trials;
-			percThreshold = new double[trialCount];
-		}
-	}
-	
-	public double mean() {
-		return StdStats.mean(percThreshold);
-	}
-	
-	public double stddev() {
-		return StdStats.stddev(percThreshold); 
-	}
-	
-	public double confidenceLo() {		
-		return (mean() - (1.96 * stddev())/(Math.sqrt(trialCount)));
-	}
-	
-	public double confidenceHi() {
-		return (mean() + (1.96 * stddev())/(Math.sqrt(trialCount)));
-	}
-	
-	public static void main (String[] args){
-		PercolationStats percStats;
-		int randomRow, randomColumn;
-		
-		if(args.length != 0){
-			percStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-		}
-		else{
-			percStats = new PercolationStats(2, 100000);
-		}
-		
-		for(int x = 0; x < percStats.trialCount; x++){
-			Percolation perc = new Percolation(percStats.gridSize);
-			
-			while(!perc.percolates()){
-				randomRow = StdRandom.uniform(percStats.gridSize) + 1;
-				randomColumn = StdRandom.uniform(percStats.gridSize) + 1;
-				
-				if(!perc.isOpen(randomRow, randomColumn)){
-					perc.open(randomRow, randomColumn);
-				}
-			}
-			double threshold = perc.numberOfOpenSites() / (double)(percStats.gridSize * percStats.gridSize);
-			percStats.percThreshold[x] = threshold;
-		}
-		
-		StdOut.println("Mean :: " + percStats.mean());
-		StdOut.println("Standard Deviation :: " + percStats.stddev());
-		StdOut.println("Confidence, low :: " + percStats.confidenceLo());
-		StdOut.println("Confidence, high :: " + percStats.confidenceHi());
-	}
+
+    private int gridSize;
+    private int trialCount;
+    private double[] percThreshold;
+
+    public PercolationStats (int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+        else {
+            gridSize = n;
+            trialCount = trials;
+            percThreshold = new double[trialCount];
+
+            int randomRow, randomColumn;
+            double threshold = 0.0;
+
+            List<Percolation> percObjects = new ArrayList<Percolation>(trialCount);
+            for (int x = 0; x < trialCount; x++) {
+                Percolation perc = new Percolation(gridSize);
+
+                percObjects.add(perc);
+
+                while (!percObjects.get(x).percolates()) {
+                    randomRow = StdRandom.uniform(gridSize) + 1;
+                    randomColumn = StdRandom.uniform(gridSize) + 1;
+
+                    if (!percObjects.get(x).isOpen(randomRow, randomColumn)) {
+                       percObjects.get(x).open(randomRow, randomColumn);
+                    }
+                }
+                threshold = percObjects.get(x).numberOfOpenSites() / (double) (gridSize * gridSize);
+                percThreshold[x] = threshold;
+            }
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(percThreshold);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(percThreshold);
+    }
+
+    public double confidenceLo() {
+        return (mean() - (1.96 * stddev())/(Math.sqrt(trialCount)));
+    }
+
+    public double confidenceHi() {
+        return (mean() + (1.96 * stddev())/(Math.sqrt(trialCount)));
+    }
+
+    public static void main(String[] args) {
+        PercolationStats percStats;
+        if(args.length != 0) {
+            percStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        }
+        else {
+            percStats = new PercolationStats(200, 100);
+        }
+
+        StdOut.println("Mean :: " + percStats.mean());
+        StdOut.println("Standard Deviation :: " + percStats.stddev());
+        StdOut.println("Confidence, low :: " + percStats.confidenceLo());
+        StdOut.println("Confidence, high :: " + percStats.confidenceHi());
+    }
 }
