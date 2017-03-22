e10691394b346e73c820db3ded2c17037136a236
AJ Birdi <angadjot@aim.com>
2017-03-22 13:36:47 -0400

    Changed formatting

--- src/Map.java
+++ src/Map.java
@@ -1,3 +1,4 @@
+//First attempt at creating Dijkstra's Algorithm
 //Way too complicated and too buggy, switched to much simpler Edge, Vertex, and Graph classes
 
 import java.security.SecureRandom;
@@ -5,9 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Map {
-	
-	//TODO Update comments/documentation
-	
+
 	private int nodeCount;
 	private SecureRandom random = new SecureRandom();
 	private ArrayList<int[]> weightedMap = new ArrayList<int[]>();
@@ -54,43 +53,25 @@ public class Map {
 					}
 					
 					if(next){
-						
-						//TODO Implement system that checks if a node has a link to a previous node so they don't double link
-									
 						tempI = i;
 						tempJ = j;
 						
 						for(int x = 0; x < j; x++){
 							arrayListIndex += edges[x];
-							System.out.println("ArrayListIndex was updated");
 						}
 						
-						//System.out.println(arrayListIndex + "\n\n");
-						
-						//while(weightedMap.get(arrayListIndex)[0] == tempI);
-						
-						//TODO Fix error where the wrong element from the array is pulled
-						
-						System.out.println("The size of the weighted map is :: " + weightedMap.size() + "\n");
-						System.out.println("ArrayListIndex is " + arrayListIndex);
-						
 						int[] array = weightedMap.get(arrayListIndex);
-						System.out.println("What the fuck is this " + array[1]);
-						
+
 						while(arrayListIterator <= edges[tempJ]){
 							if(weightedMap.get(arrayListIndex)[1] == tempI){
 								add = false;
-								System.out.println("I stopped nodes from being added :: " + Arrays.toString(weightedMap.get(arrayListIndex)));
 							}
 							else{
 								add = true;
-								System.out.println("Inside the else statement");
 							}
 							
 							if(add){
 								//If 2 nodes are connected, generate a weight between 1 and 15 then add the nodes and their weight to the map
-								
-								System.out.println("I should be adding a node");
 								weight = (random.nextInt(15)+1);
 								weightedMap.add(new int[]{i, j, weight});
 							}
@@ -98,23 +79,14 @@ public class Map {
 							arrayListIterator++;
 						}
 						
-						/*weight = (random.nextInt(15)+1);
-						weightedMap.add(new int[]{i, j, weight});
-						*/
-						
 						//Keeps track of how many edges each node has
 						edges[i]++;
 					}
 				}
 			}			
-			System.out.println("I was called");
 			System.out.println(weightedMap.size());
 			for(int i = 0; i < weightedMap.size(); i++){
 				System.out.println(Arrays.toString(weightedMap.get(i)));
 			}
-			/*for(int j = 0; j < nodeCount; j++){
-				System.out.println(j + " has " + edges[j] + " edges.");
-			}
-			*/
 		}
 }
